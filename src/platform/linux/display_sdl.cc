4b5905b987a30ffa1e31c3c80dc78c08693a97a6
Alex <21338787@studentmail.ul.ie>
2024-11-02 20:14:18 +0000

    use surface insted of texture and render so it wont use GPU

--- src/platform/linux/display_sdl.cc
+++ src/platform/linux/display_sdl.cc
@@ -32,9 +32,8 @@ struct video_app {
   bool wants_to_quit;
   std::chrono::time_point<std::chrono::system_clock> last_time;
   uint8_t rgb_data[WIN_WIDTH * WIN_HEIGHT * 3];
-  SDL_Texture *texture_rgb;
+  SDL_Surface *surface_rgb;
   SDL_Window *window;
-  SDL_Renderer *renderer;
   int win_height;
   int win_width;
 };
@@ -117,50 +116,51 @@ void createApp(video_app *self) {
   frame_rate_info.fps = plm_get_framerate(self->plm);
   frame_rate_info.total_t = plm_get_duration(self->plm);
   frame_rate_info.total_frames = frame_rate_info.total_t * frame_rate_info.fps;
-  // get the amount of time a frame can be displayed in without being dropped in
-  // ms
   frame_rate_info.frame_ms =
       (1.0 / static_cast<double>(frame_rate_info.fps)) * 1000;
 
-  // frame number true for intersection video
   std::cout << frame_rate_info.total_frames << " total frames, "
             << frame_rate_info.fps
             << "FPS, max frame time ms: " << frame_rate_info.frame_ms << "\n";
   std::cout << "correct play time sec: " << frame_rate_info.total_t << "\n";
+  SDL_Init(SDL_INIT_VIDEO);
 
   self->window = SDL_CreateWindow("pl_mpeg", SDL_WINDOWPOS_CENTERED,
                                   SDL_WINDOWPOS_CENTERED, WIN_WIDTH, WIN_HEIGHT,
-                                  SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
+                                  SDL_WINDOW_SHOWN);
 
-  self->renderer = SDL_CreateRenderer(self->window, -1, 0);
-  if (!self->renderer) {
-    std::cout << "Error: Couldn't create renderer. SDL_Error: \n"
+  self->surface_rgb = SDL_CreateRGBSurfaceWithFormat(0, WIN_WIDTH, WIN_HEIGHT,
+                                                     24, SDL_PIXELFORMAT_RGB24);
+  if (!self->surface_rgb) {
+    std::cout << "Error: Couldn't create surface. SDL_Error: \n"
               << SDL_GetError();
     self->wants_to_quit = true;
   }
 
-  self->texture_rgb =
-      SDL_CreateTexture(self->renderer, SDL_PIXELFORMAT_RGB24,
-                        SDL_TEXTUREACCESS_STREAMING, WIN_WIDTH, WIN_HEIGHT);
+  SDL_BlitSurface(self->surface_rgb, NULL, SDL_GetWindowSurface(self->window),
+                  NULL);
+
+  SDL_UpdateWindowSurface(self->window);
 }
 
 void updateFrame(plm_t *mpeg, plm_frame_t *frame, void *user) {
   auto start_time = std::chrono::high_resolution_clock::now();
   video_app *self = static_cast<video_app *>(user);
-  plm_frame_to_rgb(frame, self->rgb_data,
-                   frame->width * 3);  // can be hardware accelerated]
+  plm_frame_to_rgb(frame, self->rgb_data, frame->width * 3);
 
   auto to_rgb = std::chrono::high_resolution_clock::now();
   uint8_t new_rgb_data[N_PIXELS];
-  com::Filter::sobelEdgeDetect(self->rgb_data, N_PIXELS, frame->width * 3,
-                               new_rgb_data);
-  // com::Filter::grayscale(self->rgb_data, N_PIXELS, new_rgb_data);
+  // com::Filter::sobelEdgeDetect(self->rgb_data, N_PIXELS, frame->width * 3,
+  // new_rgb_data);
+  com::Filter::grayscale(self->rgb_data, N_PIXELS, new_rgb_data);
 
   auto to_filter = std::chrono::high_resolution_clock::now();
-  SDL_UpdateTexture(self->texture_rgb, NULL, new_rgb_data, frame->width * 3);
-  SDL_RenderClear(self->renderer);
-  SDL_RenderCopy(self->renderer, self->texture_rgb, NULL, NULL);
-  SDL_RenderPresent(self->renderer);
+  memcpy(self->surface_rgb->pixels, new_rgb_data, N_PIXELS);
+
+  SDL_BlitSurface(self->surface_rgb, NULL, SDL_GetWindowSurface(self->window),
+                  NULL);
+  SDL_UpdateWindowSurface(self->window);
+
   auto to_render = std::chrono::high_resolution_clock::now();
   ttr.push_back({start_time, to_rgb, to_filter, to_render});
   total_frames_completed++;
@@ -168,9 +168,7 @@ void updateFrame(plm_t *mpeg, plm_frame_t *frame, void *user) {
 
 void updateVideo(video_app *self) {
   auto plm_time = plm_get_time(self->plm);
-  if (plm_time >= frame_rate_info.total_t) {
-    self->wants_to_quit = true;
-  }
+
   double seek_to = -1;
   auto now = std::chrono::system_clock::now();
 
@@ -179,22 +177,9 @@ void updateVideo(video_app *self) {
 
   if (elapsed_time >= frame_rate_info.frame_ms) {
     std::cout << elapsed_time << "\n";
+    self->last_time = now;
 
-    if (elapsed_time > (frame_rate_info.frame_ms * 2)) {
-      std::cout << "tripped"
-                << "\n";
-      seek_to = plm_time +
-                ((elapsed_time - (frame_rate_info.frame_ms * 1.3)) / 1000.0);
-    }
-    if (seek_to != -1) {
-      plm_seek(self->plm, seek_to, TRUE);
-
-      self->last_time = now;
-      plm_decode(self->plm, (frame_rate_info.frame_ms / 1000.0));
-    } else {
-      self->last_time = now;
-      plm_decode(self->plm, (frame_rate_info.frame_ms / 1000.0));
-    }
+    plm_decode(self->plm, (frame_rate_info.frame_ms / 1000.0));
   }
 
   if (plm_has_ended(self->plm)) {
@@ -211,8 +196,7 @@ void updateVideo(video_app *self) {
 }  // functional but slower than nessesary
 
 void destroy(video_app *self) {
-  SDL_DestroyTexture(self->texture_rgb);
-  SDL_DestroyRenderer(self->renderer);
+  SDL_FreeSurface(self->surface_rgb);
   SDL_DestroyWindow(self->window);
   SDL_Quit();
   plm_destroy(self->plm);
