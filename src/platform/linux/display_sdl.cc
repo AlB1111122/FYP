94d278eb897ece515ee3d27235ce83af60cafe97
Alex <21338787@studentmail.ul.ie>
2024-11-06 11:43:19 +0000

    add 2 update videos, 1 that skips, one that doesnt

--- src/platform/linux/display_sdl.cc
+++ src/platform/linux/display_sdl.cc
@@ -36,6 +36,7 @@ struct video_app {
   SDL_Window *window;
   int win_height;
   int win_width;
+  bool slow_flag;
 };
 
 void createApp(video_app *self);
@@ -150,9 +151,9 @@ void updateFrame(plm_t *mpeg, plm_frame_t *frame, void *user) {
 
   auto to_rgb = std::chrono::high_resolution_clock::now();
   uint8_t new_rgb_data[N_PIXELS];
-  // com::Filter::sobelEdgeDetect(self->rgb_data, N_PIXELS, frame->width * 3,
-  // new_rgb_data);
-  com::Filter::grayscale(self->rgb_data, N_PIXELS, new_rgb_data);
+  com::Filter::sobelEdgeDetect(self->rgb_data, N_PIXELS, frame->width * 3,
+                               new_rgb_data);
+  // com::Filter::grayscale(self->rgb_data, N_PIXELS, new_rgb_data);
 
   auto to_filter = std::chrono::high_resolution_clock::now();
   memcpy(self->surface_rgb->pixels, new_rgb_data, N_PIXELS);
@@ -166,20 +167,60 @@ void updateFrame(plm_t *mpeg, plm_frame_t *frame, void *user) {
   total_frames_completed++;
 }
 
+/*void updateVideo(video_app *self) {
+  auto now = std::chrono::system_clock::now();
+
+  std::chrono::duration<double, std::milli> elapsed_tp = now - self->last_time;
+  double elapsed_time = elapsed_tp.count();
+
+  self->last_time = now;
+  plm_decode(self->plm, (frame_rate_info.frame_ms / 1000.0));
+
+  if (plm_has_ended(self->plm)) {
+    self->wants_to_quit = true;
+  }
+
+  SDL_Event ev;
+  while (SDL_PollEvent(&ev)) {
+    if (ev.type == SDL_QUIT ||
+        (ev.type == SDL_KEYUP && ev.key.keysym.sym == SDLK_ESCAPE)) {
+      self->wants_to_quit = true;
+    }
+  }
+}*/
+
 void updateVideo(video_app *self) {
   auto plm_time = plm_get_time(self->plm);
-
+  if (plm_time >= frame_rate_info.total_t) {
+    self->wants_to_quit = true;
+  }
   double seek_to = -1;
   auto now = std::chrono::system_clock::now();
 
   std::chrono::duration<double, std::milli> elapsed_tp = now - self->last_time;
   double elapsed_time = elapsed_tp.count();
 
-  if (elapsed_time >= frame_rate_info.frame_ms) {
-    std::cout << elapsed_time << "\n";
-    self->last_time = now;
+  std::cout << elapsed_time << "\n";
 
+  if (elapsed_time > (frame_rate_info.frame_ms * 2)) {
+    self->slow_flag = true;
+    if (self->slow_flag == true) {
+      // 2 slow frames in a row cause a skip
+      std::cout << "tripped"
+                << "\n";
+      seek_to = plm_time +
+                ((elapsed_time - (frame_rate_info.frame_ms * 1.2)) / 1000.0);
+    }
+  }
+  if (seek_to != -1) {
+    plm_seek(self->plm, seek_to, TRUE);  // functional but slower than nessesary
+
+    self->last_time = now;
     plm_decode(self->plm, (frame_rate_info.frame_ms / 1000.0));
+  } else {
+    self->last_time = now;
+    plm_decode(self->plm, (frame_rate_info.frame_ms / 1000.0));
+    self->slow_flag = false;
   }
 
   if (plm_has_ended(self->plm)) {
@@ -193,7 +234,7 @@ void updateVideo(video_app *self) {
       self->wants_to_quit = true;
     }
   }
-}  // functional but slower than nessesary
+}
 
 void destroy(video_app *self) {
   SDL_FreeSurface(self->surface_rgb);
