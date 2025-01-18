8fd704f59233fa8cc0af6c1c9261c9487e7915ba
Alex <21338787@studentmail.ul.ie>
2025-01-18 18:46:36 +0000

    fix accidental add

--- include/memcpyAarch64.h
+++
@@ -1,9 +0,0 @@
-#ifndef MEMCPY_AARCH64_H
-#define MEMCPY_AARCH64_H
-
-#include <stddef.h>
-
-// defined in assembly  boot.s
-extern "C" void *__memcpy_aarch64(void *dest, const void *src, size_t n);
-
-#endif // MEMCPY_AARCH64_H
